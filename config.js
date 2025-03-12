16feb20647207ac45e67c11be59f49d5ee87f15c
PTHLinh <115662191+PTHLinh@users.noreply.github.com>
2025-03-12 07:25:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   TOKEN: "",
-  language: "en",
+  language: "vi",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@cluster0.do0cdb4.mongodb.net",
+  spotifyClientId : "3dc37fb968b94de99e0779817a78ffd3",
+  spotifyClientSecret : "121d08db8e7a431db4a326cf90dbcd0b",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
